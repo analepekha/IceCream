e78df7a244a60fed997783e8e22aa1d329d37ff3
analepekha <97762889+analepekha@users.noreply.github.com>
2022-05-08 18:29:36 +0000

    Deploying to gh-pages from @ analepekha/project-IceCream@3fc0aded1bd4ec51d28110daf30a46e6975931b0 🚀

--- src.2d14f59e.js
+++ src.2d14f59e.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-IceCream/src.264c83ee.js.map
+//# sourceMappingURL=/project-IceCream/src.2d14f59e.js.map
